419f590e755c22d97372e41750cbdc9eccc948e8
Jakaria030 <gulamjakaria99@gmail.com>
2025-09-29 22:47:45 +0600

    Fix some alignment issues

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -9,7 +9,7 @@ const Faq = () => {
       <div className="mt-8 sm:mt-10 lg:mt-[120px]">
         <div className="relative">
           <div className="pr-7 mb-4 sm:mb-6 lg:mb-8 space-y-3">
-            <h2 className="text-[16px] sm:text-[20px] lg:text-[28px] font-semibold text-[#1B1743]">
+            <h2 className="text-[16px] sm:text-[20px] lg:text-[28px] font-semibold text-[#1B1743] cursor-pointer">
               Do plumbers deal with heating?
             </h2>
             <p className="text-[14px] lg:text-lg text-[#1B1743">
@@ -19,39 +19,39 @@ const Faq = () => {
           </div>
           <div className="h-[1px] bg-[#D4DBE0]"></div>
           <div className="absolute right-[2px] top-[7px]">
-            <IoIosArrowUp className="text-2xl sm:text-[32px] text-[#AAB6B5] cursor-pointer" />
+            <IoIosArrowUp className="text-2xl sm:text-[32px] text-[#AAB6B5]" />
           </div>
         </div>
         <div className="relative">
           <div className="pr-7 my-4 sm:my-6 lg:my-8">
-            <h2 className="text-[16px] sm:text-[20px] lg:text-[28px] font-semibold text-[#1B1743]">
+            <h2 className="text-[16px] sm:text-[20px] lg:text-[28px] font-semibold text-[#1B1743] cursor-pointer">
               Do you charge a call out fee?
             </h2>
           </div>
           <div className="h-[1px] bg-[#D4DBE0]"></div>
           <div className="absolute right-[2px] top-[7px]">
-            <IoIosArrowDown className="text-2xl sm:text-[32px] text-[#AAB6B5] cursor-pointer" />
+            <IoIosArrowDown className="text-2xl sm:text-[32px] text-[#AAB6B5]" />
           </div>
         </div>
         <div className="relative">
           <div className="pr-7 my-4 sm:my-6 lg:my-8">
-            <h2 className="text-[16px] sm:text-[20px] lg:text-[28px] font-semibold text-[#1B1743]">
+            <h2 className="text-[16px] sm:text-[20px] lg:text-[28px] font-semibold text-[#1B1743] cursor-pointer">
               How quickly can your company send out an engineer?
             </h2>
           </div>
           <div className="h-[1px] bg-[#D4DBE0]"></div>
           <div className="absolute right-[2px] top-[7px]">
-            <IoIosArrowDown className="text-2xl sm:text-[32px] text-[#AAB6B5] cursor-pointer" />
+            <IoIosArrowDown className="text-2xl sm:text-[32px] text-[#AAB6B5]" />
           </div>
         </div>
         <div className="relative">
           <div className="pr-7 mt-4 sm:mt-6 lg:mt-8">
-            <h2 className="text-[16px] sm:text-[20px] lg:text-[28px] font-semibold text-[#1B1743]">
+            <h2 className="text-[16px] sm:text-[20px] lg:text-[28px] font-semibold text-[#1B1743] cursor-pointer">
               What should I do if I get a water leak?
             </h2>
           </div>
           <div className="absolute right-[2px] top-[7px]">
-            <IoIosArrowDown className="text-2xl sm:text-[32px] text-[#AAB6B5] cursor-pointer" />
+            <IoIosArrowDown className="text-2xl sm:text-[32px] text-[#AAB6B5]" />
           </div>
         </div>
       </div>
